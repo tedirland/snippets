a8b36109087f33a8fc40e5cd7c20065457cf6dd7
tedirland <irlandth@gmail.com>
2023-11-28 21:20:05 -0600

    add logic to get snipped by id

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,4 +1,21 @@
-export default async function SnippetShowPage(props:any){
-    console.log(props)
-    return <div>Show a snippet</div>
+import { notFound } from "next/navigation";
+import { db } from "@/db"
+
+interface ShowPageProps {
+    params: {
+        id: string
+    }
+}
+
+export default async function SnippetShowPage(props:ShowPageProps){
+    const snippet = await db.snippet.findFirst({
+        where: {id : parseInt(props.params.id)}
+    });
+
+    if(!snippet){
+        return notFound()
+    }
+    
+   
+    return <div>{snippet.title}</div>
 }
