5b17cfd6a7870d50b176ab93730f9cf49eefe4f5
tedirland <irlandth@gmail.com>
2023-11-28 21:47:08 -0600

    style homepage

--- src/app/snippets/[id]/not-found.tsx
+++ src/app/snippets/[id]/not-found.tsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 export default function SnippetNotFound(){
     return <div>
         <h1 className="text-xl bold">
-            Hmm... that snippet was not not found.
+            Snippet not found
             Please navigate back to the home page and try again
         </h1>
-        <Link href="/">Go Home</Link>
+        <Link className="text-decoration-line: underline hover:bg-sky-700" href="/">Go Home</Link>
     </div>
-}
+} 
