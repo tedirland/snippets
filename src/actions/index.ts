ff186e36d29df5c3d5c610dd0f9979f49664afc1
tedirland <irlandth@gmail.com>
2023-11-29 21:41:17 -0600

    added error handling to create action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,54 +1,63 @@
-'use server';
+"use server";
 
 import { redirect } from "next/navigation";
 import { db } from "@/db";
 
 export async function editSnippet(id: number, code: string) {
-    await db.snippet.update({
-        where: {id},
-        data: {code},
-    });
+  await db.snippet.update({
+    where: { id },
+    data: { code },
+  });
 
-
-    redirect(`/snippets/${id}`)
+  redirect(`/snippets/${id}`);
 }
 
+export async function deleteSnippet(id: number) {
+  await db.snippet.delete({
+    where: { id },
+  });
 
-export async function deleteSnippet(id:number) {
-
-    await db.snippet.delete({
-        where: {id}
-    });
-
-    redirect("/")
-    
+  redirect("/");
 }
 
-export async function createSnippet(formState:{message:string},formData: FormData) {
-
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
     // Validate input
-    const title = formData.get('title') 
-    const code = formData.get('code')
-    if (typeof title !== 'string' || title.length <3){
-        return {
-            message: 'Title must be longer than 3 characters'
-        }
+    const title = formData.get("title");
+    const code = formData.get("code");
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer than 3 characters",
+      };
     }
-    if (typeof code !== 'string'|| code.length <10){
-        return {
-            message: 'Code must be longer'
-        }
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "Code must be longer",
+      };
     }
 
     // Create a new record in the database
 
-    const snippet = await db.snippet.create({
+    await db.snippet.create({
       data: {
         title,
-        code
-      }
-    })
-    // Redirect the user back to the root route
-    redirect("/");
-
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
+    }
   }
+  // // Redirect the user back to the root route
+  redirect("/");
+}
